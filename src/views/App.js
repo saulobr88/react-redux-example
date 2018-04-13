44f8bafd63bfaa8d476e02e750882a7fc7083932
saulobr88 <saulobr88@yahoo.com.br>
2018-04-13 11:18:50 -0300

    Back to BrowserRouter

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Contacts from './Contacts';
@@ -8,7 +8,7 @@ import Test from './Test';
 
 const App = () => {
     return(
-        <Router basename="/react-redux-example/">
+        <Router>
         <div className="App">
           <Route path='/' exact render={ () => <Home />} />
           <Route path='/contacts' exact render={ ()=> <Contacts />} />
